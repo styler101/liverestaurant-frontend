8bee47e12eb19c7c27cb06d5cbec9d7b163e1aef
Lucas <lucas.santos@mundowap.com.br>
2022-11-26 09:06:34 -0300

    refactor: remove apexcharts

--- src/features/Home/components/Bars/index.tsx
+++ src/features/Home/components/Bars/index.tsx
@@ -1,46 +1,5 @@
 import React from 'react'
-import Chart from 'react-apexcharts'
-import mock from '../../data/bars'
 
 export function Bars() {
-  const options = {
-    series: [
-      {
-        name: 'series1',
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-      {
-        name: 'series2',
-        data: [11, 32, 45, 32, 34, 52, 41],
-      },
-    ],
-    chart: {
-      height: 350,
-      type: 'area',
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: 'smooth',
-    },
-    xaxis: {
-      type: 'datetime',
-      categories: [
-        '2018-09-19T00:00:00.000Z',
-        '2018-09-19T01:30:00.000Z',
-        '2018-09-19T02:30:00.000Z',
-        '2018-09-19T03:30:00.000Z',
-        '2018-09-19T04:30:00.000Z',
-        '2018-09-19T05:30:00.000Z',
-        '2018-09-19T06:30:00.000Z',
-      ],
-    },
-    tooltip: {
-      x: {
-        format: 'dd/MM/yy HH:mm',
-      },
-    },
-  }
-  return <Chart width={300} height={300} series={options.series} type="area" />
+  return <h1> Bars</h1>
 }
