de25369138890573248bc38509849ba9c16b8062
Lucas <lucas.santos@mundowap.com.br>
2022-11-26 10:10:17 -0300

    feat: add home options

--- src/features/Home/ui/index.tsx
+++ src/features/Home/ui/index.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Dropdown, Popup } from 'semantic-ui-react'
+import { FiMoreVertical, FiEye, FiEyeOff } from 'react-icons/fi'
+import { BsFillGridFill, BsGrid3X3GapFill } from 'react-icons/bs'
 import { Header } from '@/components/Header'
-import { Columns } from '@/components/Graphics'
+import useOnClickOutSide from '@/hooks/useClickoutSide'
 import * as S from './styles'
 
 export function Ui() {
+  const [activeOptions, setActiveOptions] = useState(false)
+  const dropRef = useOnClickOutSide(() => setActiveOptions(false))
   return (
     <S.Container>
       <Header
         title="Painel de Atendimento"
         description="Utilize os recuros abaixo para monitorar o faturamento do restaurante"
       />
-      <Columns />
+      <S.Wrapper>
+        <S.Options>
+          <Popup
+            trigger={
+              <button type="button" onClick={() => setActiveOptions(true)}>
+                Modo de Exibição
+                {activeOptions ? <FiEye /> : <FiEyeOff />}
+              </button>
+            }
+            position="bottom center"
+            on="click"
+            open={activeOptions}
+            content={
+              <S.OptionsContainer ref={dropRef}>
+                <BsFillGridFill size={25} color="#1a1a1d" />
+                <BsGrid3X3GapFill size={25} color="#1a1a1d" />
+              </S.OptionsContainer>
+            }
+          />
+
+          <Dropdown
+            icon={<FiMoreVertical color="#1a1a1d" size={20} />}
+            floating
+          >
+            <Dropdown.Menu direction="left">
+              <Dropdown.Item text="Exportar em PDF" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </S.Options>
+      </S.Wrapper>
     </S.Container>
   )
 }
--- src/features/Home/ui/styles.ts
+++ src/features/Home/ui/styles.ts
@@ -4,4 +4,39 @@ export const Container = styled.div`
   flex: 1;
 `
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+`
+
+export const Options = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  svg {
+    cursor: pointer;
+  }
+
+  button {
+    font-size: 18px;
+    border: 0;
+    color: #c8c8c8;
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-left: 8px;
+    }
+  }
+`
+
+export const OptionsContainer = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
