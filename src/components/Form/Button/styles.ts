b13b8a4e38511bb818d8a08bff6487fb5d1550eb
Lucas <lucas.santos@mundowap.com.br>
2022-11-29 20:34:12 -0300

    fix: sigin button styles

--- src/components/Form/Button/styles.ts
+++ src/components/Form/Button/styles.ts
@@ -4,24 +4,23 @@ interface ContainerProps {
   color: string
 }
 export const Container = styled.button<ContainerProps>`
- width:100%;
- height:42px;
- border-radius: 4px;
- margin-top: 8px;
- background-color: ${(props) => props.color};
- border:0;
- cursor: pointer;
+  width: 100%;
+  height: 42px;
+  border-radius: 4px;
+  margin-top: 8px;
+  background-color: ${(props) => props.color};
+  border: 0;
+  cursor: pointer;
 
+  &:disabled {
+    background-color: #eee;
+    filter: none;
+    span {
+      color: #ccc !important;
+    }
+  }
 
- &:disabled{
-  background-color:#eee;
-  filter: none
- }
-
- &:hover{
-  filter: brightness(96%);
-
- }
-
-
+  &:hover {
+    filter: brightness(96%);
+  }
 `
